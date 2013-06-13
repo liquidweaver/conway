fc99bcd384f6308cc87e6c7161149d07222f7b03
Joshua Weaver <joshuaweaver@gmail.com>
2013-06-13 17:06:21 -0500

    Added Cell and tests

--- cell.rb
+++ cell.rb
@@ -1,2 +1,26 @@
 class Cell
+  class StateInvalid < Exception; end
+  VALID_STATES = [ :alive, :dead ]
+
+  attr_accessor :state
+
+  def initialize(initial_state=:alive)
+    @state = initial_state
+  end
+
+  def state=( new_state )
+    raise StateInvalid, "State must be :alive or :dead" unless 
+      VALID_STATES.include? new_state
+
+    @state = new_state
+  end
+
+  def dead?
+    @state == :dead
+  end
+
+  def alive?
+    @state == :alive
+  end
+
 end
---
+++ cell_spec.rb
@@ -0,0 +1,30 @@
+require_relative 'cell'
+
+describe Cell do
+  it { should respond_to :state }
+
+  it "allows :alive and :dead for states" do
+    expect { subject.state = :alive }.to_not raise_error
+    expect { subject.state = :dead }.to_not raise_error
+  end
+
+  it "raises an exception for invalid state values" do 
+    expect { subject.state = :happy }.to raise_error Cell::StateInvalid
+    expect { subject.state = 34 }.to raise_error Cell::StateInvalid
+  end
+
+  context "when alive" do
+    subject { Cell.new :alive }
+
+    its(:state) { should eq :alive }
+    it { should be_alive }
+  end
+
+  context "when dead" do
+    subject { Cell.new :dead }
+
+    its(:state) { should eq :dead }
+    it { should be_dead }
+  end
+
+end
